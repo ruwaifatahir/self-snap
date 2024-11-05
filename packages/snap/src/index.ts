c15e47cb20792bc9f4177cc25ad3ee7d9f0c33f5
ruwaifatahir <66880814+ruwaifatahir@users.noreply.github.com>
2024-11-05 07:51:49 +0500

    add snap

---
+++ packages/snap/src/index.ts
@@ -0,0 +1,60 @@
+import type { OnNameLookupHandler } from '@metamask/snaps-sdk';
+
+/**
+ * Handle incoming name lookup requests from the MetaMask clients.
+ *
+ * @param request - The request arguments.
+ * @param request.domain - The domain to resolve. Will be undefined if an address is provided.
+ * @param request.address - The address to resolve. Will be undefined if a domain is provided.
+ * @param request.chainId - The CAIP-2 chain ID of the associated network.
+ * @returns If successful, an object containing the resolvedDomain or resolvedAddress. Null otherwise.
+ */
+
+export const onNameLookup: OnNameLookupHandler = async (request) => {
+  const { chainId: chainIdEip155, address, domain } = request;
+
+  const name = domain?.slice(2) || '';
+  const chainId = chainIdEip155.slice(7);
+
+  if (address) {
+    return null;
+  }
+
+  if (domain) {
+    try {
+      const response = await fetch(
+        `https://self-name-res-api.vercel.app/api/resolveName?name=${name}&chainId=${chainId}`,
+        {
+          headers: {
+            accept: 'application/json',
+            authorization: '111112',
+          },
+        },
+      );
+
+      if (!response.ok) {
+        console.error('Fetch error:', response.status, response.statusText);
+
+        return null;
+      }
+
+      const data = await response.json();
+
+      if (data.address) {
+        return {
+          resolvedAddresses: [
+            {
+              resolvedAddress: data.address,
+              protocol: 'Self name resolution',
+              domainName: domain,
+            },
+          ],
+        };
+      }
+    } catch (error) {
+      console.error('Error parsing data or network issues:', error);
+    }
+  }
+
+  return null;
+};
